dfb5566e3d95d87bdf80f6b91c7426bc77748a25
yusinKim <shinkim7941@gmail.com>
2023-02-24 16:30:46 +0900

    toolbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,43 +1,27 @@
 <template>
 
-<!-- header colour: #145de7 -->
-  <div class="nav-container">
-    <!-- <v-app-bar-nav-icon @click="drawer = !drawer" />
-    <v-navigation-drawer v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6"> Dashboard </v-list-item-title>
-          <v-list-item-subtitle> Navigation drawers</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer> -->
-    <v-toolbar title="my weather" extended>
-      <v-app-bar-title class="title">my weather</v-app-bar-title>
+  <v-card
+    color="grey-lighten-4"
+    flat
+    height="200px"
+    rounded="0"
+  >
+    <v-toolbar title="my weather" flat outlined>
+      <v-toolbar-title class="title">
+        my weather :)
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
         <v-btn
-          flat
+          class="align-center"
+          text
           v-for="item in items"
           :key="item.title"
           >
-          <v-icon left dark>{{ item.icon }}</v-icon>
-          {{ item.title }}
+          <v-icon class="align-start">{{ item.icon }}</v-icon>
+          <span>{{ item.title }}</span>
         </v-btn>
-      </v-toolbar-items>
     </v-toolbar>
-  </div>
+  </v-card>
 </template>
 
 <script>
@@ -46,7 +30,8 @@ export default {
     return {
       drawer : false,
       items: [
-        { title: 'dayily note', icon: 'fa-solid fa-book' },
+        { title: 'dayily', icon: 'fa-solid fa-book' },
+        { title: 'weather', icon: 'fa-solid fa-cloud' }
       ],
       rail: true,
     }
@@ -58,5 +43,9 @@ export default {
 <style scoped>
 .title {
   color: #145de7;
+  font-weight: 900;
+}
+.v-app-bar-title__content {
+  width: 200px;
 }
 </style>
